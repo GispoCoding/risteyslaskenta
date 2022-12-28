5821933a4c2857389cfdf1a005cc98db4f04d440
Niko Aarnio <niko.aarnio@protonmail.com>
2022-12-28 09:38:24 +0200

    output features are curves, less circular artifact geometries, v desgined for fixed width visuals

--- risteyslaskenta_package/risteyslaskenta_functions.py
+++ risteyslaskenta_package/risteyslaskenta_functions.py
@@ -1,10 +1,14 @@
-from functools import reduce
+from math import sqrt
+from typing import Tuple
 
+import numpy as np
 from qgis.core import (
+    QgsCircularString,
     QgsFeature,
     QgsField,
     QgsGeometry,
     QgsPalLayerSettings,
+    QgsPoint,
     QgsPointXY,
     QgsProject,
     QgsTextBufferSettings,
@@ -18,7 +22,7 @@ from qgis.PyQt.QtGui import QColor, QFont
 
 
 def create_result_layer(crs) -> QgsVectorLayer:
-    result_layer = QgsVectorLayer("LineString", "temp", "memory")
+    result_layer = QgsVectorLayer("CompoundCurve", "temp", "memory")
     result_layer.setCrs(crs)
     result_layer.dataProvider().addAttributes(
         [
@@ -32,6 +36,25 @@ def create_result_layer(crs) -> QgsVectorLayer:
     result_layer.setName("Intersections visualized")
     QgsProject.instance().addMapLayer(result_layer)
     return result_layer
+
+
+def perpendicular(coords: Tuple[float, float]):
+    perpendicular_vector = np.empty_like(coords)
+    perpendicular_vector[0] = -coords[1]
+    perpendicular_vector[1] = coords[0]
+    return perpendicular_vector
+
+
+def normalize(array):
+    array = np.array(array)
+    return array / np.linalg.norm(array)
+
+
+def distance(p1: Tuple[float, float], p2: Tuple[float, float]) -> float:
+    x = (p1[0] - p2[0]) ** 2
+    y = (p1[1] - p2[1]) ** 2
+    distance = sqrt(x + y)
+    return distance
 
 
 # def convert_polygons_to_centroids(polygon_layer: QgsVectorLayer) -> QgsVectorLayer:
@@ -70,12 +93,16 @@ def check_same_crs(layer1, layer2) -> bool:
 
 
 def calculate_middle_point(
-    start_point: QgsPointXY, end_point: QgsPointXY, intersection_middle_point
+    start_point: QgsPointXY,
+    end_point: QgsPointXY,
+    intersection_middle_point,
+    straight_road=False,
 ) -> QgsPointXY:
     mid_x = (start_point.x() + end_point.x()) / 2
     mid_y = (start_point.y() + end_point.y()) / 2
+
     # If distance to centre is more than 2m, likely a turn
-    if abs(mid_x - intersection_middle_point[0]) > 2:
+    if not straight_road:
         mid_x = (mid_x + intersection_middle_point[0]) / 2
         mid_y = (mid_y + intersection_middle_point[1]) / 2
     return QgsPointXY(mid_x, mid_y)
@@ -97,39 +124,97 @@ def create_lines_for_intersection(
     if len(location_feats) == 0:
         return False
 
+    nr_of_branches = max([int(feat["Haara"]) for feat in location_feats])
+    moved_list = []
+
     for data_feat in data_feats:
         start_point, end_point = None, None
+        x_coords = set([feat.geometry().asPoint().x() for feat in location_feats])
+        y_coords = set([feat.geometry().asPoint().y() for feat in location_feats])
+        intersection_center_point = sum(x_coords) / len(x_coords), sum(y_coords) / len(
+            y_coords
+        )
 
-        intersection_branches = []
         for location_feat in location_feats:
-            intersection_branches.append(
-                (
-                    location_feat.geometry().asPoint().x(),
-                    location_feat.geometry().asPoint().y(),
-                )
-            )
 
             if str(location_feat["Haara"]) == data_feat["direction"][0]:
                 start_point = location_feat.geometry().asPoint()
             elif str(location_feat["Haara"]) == data_feat["direction"][1]:
                 end_point = location_feat.geometry().asPoint()
+            if start_point and end_point:
+                break
+
+        if (
+            nr_of_branches == 4
+            and abs(int(data_feat["direction"][0]) - int(data_feat["direction"][1]))
+            == 2
+        ):
+            straight_road = True
+        else:
+            straight_road = False
 
         if start_point and end_point:
-            intersection_center_point = reduce(
-                lambda point1, point2: (
-                    (point1[0] + point2[0]) / 2,
-                    (point1[1] + point2[1]) / 2,
-                ),
-                intersection_branches,
+            # Calculate middle point for curve
+            middle_point = calculate_middle_point(
+                start_point, end_point, intersection_center_point, straight_road
             )
-            line_middle_point = calculate_middle_point(
-                start_point, end_point, intersection_center_point
+            # If the intersection branches are oddly placed, we need to make correction
+            # If this was not done, some intersections would have large round curves
+            if distance(
+                (middle_point.x(), middle_point.y()), (end_point.x(), end_point.y())
+            ) > distance(
+                (start_point.x(), start_point.y()), (end_point.x(), end_point.y())
+            ):
+                mid_x = (start_point.x() + end_point.x()) / 2
+                mid_y = (start_point.y() + end_point.y()) / 2
+                middle_point = QgsPointXY(mid_x, mid_y)
+
+            # Calculate perpendicular and normalized vector of a straight line
+            # from intersection branch to another
+            unit_vector = normalize(
+                perpendicular(
+                    (end_point.x() - start_point.x(), end_point.y() - start_point.y())
+                )
+            )
+
+            if straight_road:
+                move_vector = 2 * unit_vector
+            else:
+                # Move the start and end points away from intersection center
+                # First make sure the direction is away from the intersection center
+                if distance(
+                    (start_point.x(), start_point.y()), intersection_center_point
+                ) > distance(
+                    (
+                        start_point.x() + unit_vector[0],
+                        start_point.y() + unit_vector[1],
+                    ),
+                    intersection_center_point,
+                ):
+                    unit_vector = -unit_vector
+                # If the curve being drawn is 2nd for a branch pair, move it further
+                if data_feat["direction"][::-1] in moved_list:
+                    move_vector = 10 * unit_vector
+                else:
+                    move_vector = 6 * unit_vector
+                    moved_list.append(data_feat["direction"])
+            start_point = QgsPointXY(
+                start_point.x() + move_vector[0], start_point.y() + move_vector[1]
+            )
+            middle_point = QgsPointXY(
+                middle_point.x() + move_vector[0], middle_point.y() + move_vector[1]
+            )
+            end_point = QgsPointXY(
+                end_point.x() + move_vector[0], end_point.y() + move_vector[1]
             )
 
             feat = QgsFeature(result_layer.fields())
-            feat.setGeometry(
-                QgsGeometry.fromPolylineXY([start_point, line_middle_point, end_point])
+            circular_ring = QgsCircularString(
+                QgsPoint(start_point), QgsPoint(middle_point), QgsPoint(end_point)
             )
+            geom = QgsGeometry(circular_ring)
+            feat.setGeometry(geom)
+
             feat.setAttributes(
                 [
                     data_feat["fid"],
